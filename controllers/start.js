c6ba32d9c01a2ff354cced2e93044e797819e18b
(20090004) Lee Fitzgerald <20090004@setu.ie>
2026-02-12 16:58:33 +0000

    Handlebars added, finish lab - Step 7

--- controllers/start.js
+++ controllers/start.js
@@ -1,13 +1,19 @@
-'use strict'
+'use strict';
 
-import logger from '../utils/logger.js';
+import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
 
-const start = 
-{
-  createView(request, response)
-  {
-    logger.info('Start page Loading...');
-    response.send("Welcome to Lee's Playlist App!");
+const start = {
+  createView(request, response) {
+    logger.info("Start page loading!");
+    
+    const viewData = {
+      title: "Welcome to the Playlist app!",
+      info: appStore.getAppInfo()
+    };
+    
+    //logger.debug(viewData);
+    response.render('start', viewData);   
   },
 };
 
