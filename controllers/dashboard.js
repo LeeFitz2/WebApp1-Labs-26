ecf588e7cc661ecc783d6e3e694c3493faa47146
Lee Fitzgerald <leefitzgerald13@gmail.com>
2026-04-14 13:32:34 +0100

    Some files not saved

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -23,40 +23,26 @@ const dashboard =
 
   addPlaylist(request, response) 
   {
-<<<<<<< HEAD
     const timestamp = new Date();
 
-=======
->>>>>>> 319e14734ea7ccebc984d21092c099888a051b76
     const newPlaylist = 
     {
       id: uuidv4(),
       title: request.body.title,
-<<<<<<< HEAD
       date: timestamp,
-=======
->>>>>>> 319e14734ea7ccebc984d21092c099888a051b76
       songs: [],
     };
     playlistStore.addPlaylist(newPlaylist);
     response.redirect('/dashboard');
   },
 
-<<<<<<< HEAD
   deletePlaylist(request,response)
-=======
-  deletePlaylist(request, response)
->>>>>>> 319e14734ea7ccebc984d21092c099888a051b76
   {
     const playlistId = request.params.id;
     logger.debug(`Deleting Playlist ${playlistId}`);
     playlistStore.removePlaylist(playlistId);
     response.redirect("/dashboard");
-<<<<<<< HEAD
   }
-=======
-  },
->>>>>>> 319e14734ea7ccebc984d21092c099888a051b76
 };
 
 export default dashboard;
--- models/playlist-store.js
+++ models/playlist-store.js
@@ -18,19 +18,6 @@ const playlistStore = {
         return this.store.findOneBy(this.collection, (playlist => playlist.id === id));
     },
 
-<<<<<<< HEAD
-=======
-    addSong(id, song)
-    {
-        this.store.addItem(this.collection, id, this.array, song);
-    },
-
-    removeSong(id, songId)
-    {
-        this.store.removeItem(this.collection, id, this.array, songId);
-    },
-
->>>>>>> 319e14734ea7ccebc984d21092c099888a051b76
     addPlaylist(playlist)
     {
         this.store.addCollection(this.collection, playlist);
@@ -41,7 +28,6 @@ const playlistStore = {
         const playlist = this.getPlaylist(id);
         this.store.removeCollection(this.collection, playlist);
     },
-<<<<<<< HEAD
 
     addSong(id, song)
     {
@@ -52,8 +38,6 @@ const playlistStore = {
     {
         this.store.removeItem(this.collection, id, this.array, songId);
     },
-=======
->>>>>>> 319e14734ea7ccebc984d21092c099888a051b76
 };
 
 export default playlistStore;
